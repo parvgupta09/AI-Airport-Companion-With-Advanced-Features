abef30d63030580c3d10261a3bb2ca98b393cbdc
Parv Gupta <parvgupta@Parvs-MacBook-Air.local>
2026-08-26 09:23:46 +0530

    Redis Client converted from local to cloud redis

--- backend/app/core/redis_client.py
+++ backend/app/core/redis_client.py
@@ -5,12 +5,14 @@ from app.core.config import REDIS_URL
 
 logger = logging.getLogger(__name__)
 
-REDIS_URL = os.getenv("REDIS_URL", REDIS_URL)
-
 redis_pool = redis.from_url(
     REDIS_URL,
     decode_responses = True,
-    max_connections = 20
+    max_connections = 20,
+    socket_timeout=5.0,
+    socket_connect_timeout=5.0,
+    retry_on_timeout=True,
+    health_check_interval=30
 )
 
 async def get_redis() -> redis.Redis:
